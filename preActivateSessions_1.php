7e1e6905ec5f7f84780bccdd5d90e7fe3538146a
Amrit Gautam <gautamamrit24@gmail.com>
2016-12-27 05:04:24 +0200

    Update comments

--- preActivateSessions_1.php
+++ preActivateSessions_1.php
@@ -1,8 +1,11 @@
 <?php 
     require("config.php");
-
-    // Users whose session will start automatically
-    $preActivateSessionUsers = ["amritg","admin","a"];
+    /* 
+        Everytime when any users login in successfully,
+        1. Loop through all the users, whose session is to be pre-activated, in order to Check if the session has already started.
+        2. If there is no pre-activated session, create session for each users by setting the session_id(), incrementing interger number by 5, fetched from db.
+    */
+    $preActivateSessionUsers = ["amritg","admin","a"]; // Users whose session will start automatically
 
     for($i = 0;$i < (count($preActivateSessionUsers)); $i++){
         $sessionUser = "sess_" . bin2hex($preActivateSessionUsers[$i]); // Encode Users in hexcode
@@ -11,6 +14,8 @@
             $rowUser = mysqli_fetch_array($searchUser, MYSQLI_ASSOC);
             if($rowUser){
                 $userName = $rowUser["name"];
+                
+                // Create new session
                 session_id(bin2hex($userName));
                 // echo session_id()+"<br>";
                 session_start();
@@ -20,9 +25,9 @@
             }
         }
     }
-    // Checks if the session has started or not for Users who Session is to be activated automatically!! 
+    // chechStoredSession function loop through all the stored session to check if the session has already be set or not for Users where $sessionVariable(userName) is to be set automatically!!
     function checkStoredSession($sessionId){
-        $allStoredSession = scandir(session_save_path());
+        $allStoredSession = scandir(session_save_path()); // return arrayList of all file names inside the directroy where all sessions are stored
         for($j = 0; $j < (count($allStoredSession)); $j++){
             $pSession = $allStoredSession[$j];
             if($pSession == $sessionId){
